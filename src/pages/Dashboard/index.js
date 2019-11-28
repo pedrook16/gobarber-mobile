379ced3017da7650cba18afc6fe33d5f70fd9c86
Pedro Henrique <pedrohenrique@iMac-de-Pedro.local>
2019-11-27 21:22:16 -0300

    add

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,57 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+
+import Background from '~/components/Background';
+import Appointment from '~/components/Appointment';
+
+import { Container, Title, List } from './styles';
 
 export default function Dashboard() {
-  return <View />;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointment() {
+      const response = await api.get('appointments');
+      setData(response.data);
+    }
+    loadAppointment();
+  }, []);
+
+  async function handleCancel(id) {
+    const response = await api.delete(`appointments/${id}`);
+
+    setData(
+      data.map(appointment =>
+        appointment.id === id
+          ? {
+              ...appointment,
+              canceled_at: response.data.canceled_at,
+            }
+          : appointment
+      )
+    );
+  }
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+        <List
+          data={data}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Appointment OnCancel={() => handleCancel(item.id)} data={item} />
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamentos',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
